e33dda19cce3f973dcac83441e4a3ca0d9e53e52
mutiairnawati <30835914+mutiairnawati@users.noreply.github.com>
2019-06-17 23:34:31 +0700

    Update AddEvent.php

--- AddEvent.php
+++ AddEvent.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 
 // Connexion à la base de données
 require_once('bdd.php');
@@ -9,3 +9,26 @@ if (isset($_POST['title']) && isset($_POST['start']) && isset($_POST['end']) && 
  $start = $_POST['start'];
  $end = $_POST['end'];
  $color = $_POST['color'];
+
+ $sql = "INSERT INTO events(title, start, end, color) values ('$title', '$start', '$end', '$color')";
+ //$req = $bdd->prepare($sql);
+ //$req->execute();
+ 
+ echo $sql;
+ 
+ $query = $bdd->prepare( $sql );
+ if ($query == false) {
+  print_r($bdd->errorInfo());
+  die ('Erreur prepare');
+ }
+ $sth = $query->execute();
+ if ($sth == false) {
+  print_r($query->errorInfo());
+  die ('Erreur execute');
+ }
+
+}
+header('Location: '.$_SERVER['HTTP_REFERER']);
+
+ 
+?>
